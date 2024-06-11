991719c7aa0e68a785b95ff4a02d4db4b926d052
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-06-11 11:16:43 -0400

    added

--- 0-fetching-demo-start/js/event-handlers.js
+++ 0-fetching-demo-start/js/event-handlers.js
@@ -1,3 +1,4 @@
+//Function that appends dog img to DOM
 import { setDogPicture } from "./dom-helpers.js"
 
 export const fetchNewDog = () => {
@@ -7,17 +8,36 @@ export const fetchNewDog = () => {
   // 3. parse the response body
   // 4. use the response body to set the dog picture
   // 5. handle errors
-  fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to fetch a new dog');
-      }
-      return response.json();
-    })
-    .then(data => {
-      setDogPicture(data.message);
-    })
-    .catch(error => {
-      console.error(error);
-    })
 }
+
+//POST
+
+const newUser = { name: "morpheus", job: "leader" };
+// const updateUser = {name: "neo", job: "leader"}
+const postOption = {
+  method: "POST",                      // The type of HTTP request
+  body: JSON.stringify(newUser),       // The data to be sent to the API
+  headers: {
+    "Content-Type": "application/json" // The format of the body's data
+  }  
+}
+
+// This is a good API to practice GET/POST/PATCH/DELETE requests
+const url = 'https://reqres.in/api/users';
+
+// Notice that the options object is provided as the second arg
+fetch(url, postOption)
+  .then((response) => {
+    if (!response.ok) {
+      return console.log(`Fetch failed. ${response.status} ${response.statusText}`)
+    }
+    return response.json();
+  })
+  .then((responseData) => {
+      // A POST request will return the object created with an id and a createdAt timestamp
+      console.log("Here is your data:", responseData);
+  })
+  .catch((error) => {
+    console.log("Error caught!");
+    console.error(error.message);
+  })
